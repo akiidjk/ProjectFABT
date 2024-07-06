7c6dccdd4a530798a7f14af0c53f84df258bcb4b
Retr0 <dragonfaiver@gmail.com>
2024-07-06 13:41:23 +0200

    Update some scripts

--- setup.sh
+++ setup.sh
@@ -1,5 +1,22 @@
+#!/bin/bash
 echo "Setting up the environment..."
-chmod +x ./main.py
-sudo mv ./main.py /usr/local/bin/fabt
+
+pythonScriptPath="./main.py"
+batFileName="fabt"
+targetPath="/usr/local/bin"
+
+currentDir=$(pwd)
+
+pythonScriptFullPath="$currentDir/$pythonScriptPath"
+
+if [ ! -d "$targetPath" ]; then
+    sudo mkdir -p "$targetPath"
+fi
+
+echo "#!/bin/bash" > "$targetPath/$batFileName"
+echo "python3 \"$pythonScriptFullPath\" \"\$@\"" >> "$targetPath/$batFileName"
+
+sudo chmod +x "$targetPath/$batFileName"
+
 echo "Done! Run 'fabt -v' to start the program."
 
